e7c986c8cab7f0d8477d75f13ec6041c52110a40
bimov <dmitbay123@gmail.com>
2025-07-02 00:32:38 +0300

    add test add compare

--- src/main/java/store/Citilink/elements/ProductCardElement.java
+++ src/main/java/store/Citilink/elements/ProductCardElement.java
@@ -5,7 +5,7 @@ package store.Citilink.elements;
  * Содержит методы для добавления товара в корзину, сравнение и избранное.
  */
 public class ProductCardElement extends ProductSnippetElement {
-    private static final String CARD_BY_NAME_XPATH = SNIPPET_BY_NAME_XPATH.replaceFirst("%s", "SnippetProductVerticalLayout");
+    private static String CARD_BY_NAME_XPATH;
 
     /**
      * Приватный конструктор.
@@ -23,8 +23,19 @@ public class ProductCardElement extends ProductSnippetElement {
      * @param productName точное название товара
      * @return новый объект ProductCardElement
      */
-    public static ProductCardElement byName(String productName) {
+    public static ProductCardElement byName(String snippetType, String productName) {
+        CARD_BY_NAME_XPATH = SNIPPET_BY_NAME_XPATH.replaceFirst("%s", snippetType);
         return new ProductCardElement(CARD_BY_NAME_XPATH, productName);
+    }
+
+    /**
+     * Фабричный метод для поиска карточки товара по значению атрибута data-meta-name.
+     *
+     * @param snippetType значение атрибута data-meta-name
+     * @return новый объект ProductCardElement, соответствующий найденному элементу
+     */
+    public static ProductCardElement byDataMetaName(String snippetType) {
+        return new ProductCardElement(DATA_META_NAME_XPATH, snippetType);
     }
 
     /**
